c0f11f77a6b2e904662b5bce26a648112a1990c4
Arfanur Rahman <arfanur03@gmail.com>
2022-01-31 14:36:03 +0600

    array-index

--- array-index.js
+++ array-index.js
@@ -1,1 +1,23 @@
-var bookPages = ['hablu', 'bolod', 'chouddogosti', 'battary', 'paracitamol', 'gymnesium']
+var books = ['hablu', 'bolod', 'choddugusti', 'battery', 'paracitamol', 'gymnsium'];
+
+var batteryIndex = books.indexOf('battery');
+// console.log(batteryIndex);
+
+var numbers = [45, 98, 65, 72, 52, 63];
+var index = numbers.indexOf(52);
+var index = numbers.indexOf(159);
+// console.log(index);
+
+// books = books[5];
+// console.log(books);
+
+// var secondIndex = books[119];
+// console.log(secondIndex);
+
+console.log(numbers);
+numbers[1] = 95;
+
+console.log(numbers);
+numbers[4] = 21;
+
+console.log(numbers);
