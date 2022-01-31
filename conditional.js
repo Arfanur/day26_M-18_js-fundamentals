a3c4ad57263a0276111acf38ca7d7ac2052db300
Arfanur Rahman <arfanur03@gmail.com>
2022-01-31 21:48:07 +0600

    nested-condition

---
+++ conditional.js
@@ -0,0 +1,38 @@
+var isFoodReady = false;
+// jodi food ready hoy taile ami khamu
+// if food is ready i will eat
+// if food is Read == true
+// if (condition) {
+
+// }
+if (isFoodReady == true) {
+console.log('Mama!! I will eat now.');
+};
+
+var iphonePrice = 70000;
+var myBudget = 109500;
+
+// if (iphonePrice < myBudget) {
+//     console.log('Iphone diya pic tule futani marbo!!');
+// }
+
+// if (iphonePrice > myBudget) {
+//     console.log('My Xiaomi is the best');
+// }
+
+// if (condition) {
+//     //
+// }
+// else {
+
+// }
+
+var chickenPrice = 180;
+var myMoney = 50;
+
+if (chickenPrice < myMoney) {
+    console.log('Yes! murgir Ran Khamu ar haddi chabamu');
+}
+else {
+    console.log('Smashed potato with Lentil soup with well cooked rice');
+}
---
+++ nested-condition.js
@@ -0,0 +1,27 @@
+var danishPrice = 50;
+var butterBreadPrice = 25;
+var toastBiscuitPrice = 10;
+var myBudget = 57;
+var packedWell = false;
+
+// if (danishPrice < myBudget) {
+//     console.log('Danish kheye danish jabo');
+// }
+// else if (butterBreadPrice < myBudget) {
+//     console.log('Butter bon diye cha khamu');
+// }
+// else if (toastBiscuitPrice < myBudget) {
+//     console.log('toast biscuit khamu');
+// }
+// else {
+//     console.log('Batasha diye cha khamu');
+// }
+
+if (danishPrice < myBudget) {
+    if (packedWell == true) {
+        console.log('danish khabo');
+    }
+    else {
+        console.log('danish khabo na! karon ami khaoyar age machi kheye felse ordhek');
+    }
+}
